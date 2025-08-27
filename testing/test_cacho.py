7b5bed74a3611114a396a88a8507cff2892f5e26
Nikolexion <nfalc2003@gmail.com>
2025-08-27 12:54:54 -0400

    Red: test para recuperar la cantidad actual de dados en el cacho - falla como esperado

--- testing/test_cacho.py
+++ testing/test_cacho.py
@@ -4,3 +4,7 @@ def test_agitar_cacho():
     cacho = Cacho()
     resultado = cacho.agitar()
     assert resultado is not None
+
+def test_numero_dados_cacho():
+    cacho = Cacho()
+    assert cacho.numero_dados() == 5
