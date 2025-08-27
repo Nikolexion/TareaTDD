d84ee60b9df0132614f6391ca8c529c6d7ba59af
Nikolexion <nfalc2003@gmail.com>
2025-08-26 23:28:57 -0400

    Refactor: mejorado y generalizado el codigo de lanzar dado

--- src/juego/dado.py
+++ src/juego/dado.py
@@ -1,7 +1,9 @@
+import random
+
 class Dado:
     def __init__(self):
         self.valor = None
     
     def lanzar(self):
-        self.valor = 1
+        self.valor = random.randint(1, 6)
         return self.valor
