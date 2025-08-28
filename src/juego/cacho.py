6ece7c0b9b49f35dbe1dc260b3630479d56cc63d
Nikolexion <nfalc2003@gmail.com>
2025-08-28 11:16:07 -0400

    fix: ahora esta mas ordenado cacho.py ademas de mantener las instancias de dado como tal en vez de reemplazarlos con valores

--- src/juego/cacho.py
+++ src/juego/cacho.py
@@ -3,24 +3,21 @@ from .dado import Dado
 class Cacho:
     def __init__(self):
         self.dado1 = Dado()
-        self.dado1.lanzar()
         self.dado2 = Dado()
-        self.dado2.lanzar()
         self.dado3 = Dado()
-        self.dado3.lanzar()
         self.dado4 = Dado()
-        self.dado4.lanzar()
         self.dado5 = Dado()
-        self.dado5.lanzar()
 
         self.dados = [self.dado1, self.dado2, self.dado3, self.dado4, self.dado5]
+        for dado in self.dados:
+            dado.lanzar()
         self.num_dados = 5
 
     def agitar(self):
         if self.num_dados == 0:
             return None
-        for i in range(len(self.dados)):
-            self.dados[i] = self.dados[i].lanzar()
+        for dado in self.dados:
+            dado.lanzar()
         return self.dados
     
     def numero_dados(self):
