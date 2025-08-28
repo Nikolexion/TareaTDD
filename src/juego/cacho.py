b5bef4052f627c24746209d549e6af690158c624
Nikolexion <nfalc2003@gmail.com>
2025-08-28 10:04:24 -0400

    Refactor: implementada una funcion para devolver el numero real de dados en el juego

--- src/juego/cacho.py
+++ src/juego/cacho.py
@@ -3,10 +3,16 @@ from .dado import Dado
 class Cacho:
     def __init__(self):
         self.dado1 = Dado()
+        self.dado1.lanzar()
         self.dado2 = Dado()
+        self.dado2.lanzar()
         self.dado3 = Dado()
+        self.dado3.lanzar()
         self.dado4 = Dado()
+        self.dado4.lanzar()
         self.dado5 = Dado()
+        self.dado5.lanzar()
+
         self.dados = [self.dado1, self.dado2, self.dado3, self.dado4, self.dado5]
         self.num_dados = 5
 
@@ -19,6 +25,9 @@ class Cacho:
     
     def numero_dados(self):
         return self.num_dados
+
+    def numero_dados_en_juego(self):
+        return len(self.dados)
     
     def quitar_dado(self):
         if self.num_dados > 5:
