42930e8614e47e35d10757dc127d42a42f61da11
Nikolexion <nfalc2003@gmail.com>
2025-08-28 18:27:56 -0400

    Refactor: generalizado el contador de pintas para todos los casos

--- src/juego/contador_pintas.py
+++ src/juego/contador_pintas.py
@@ -1,25 +1,19 @@
-class Contador_pintas:
-    def __init__(self, obligar, cacho):
-        self.obligar = obligar
-        self.cacho = cacho
-        self.contador = [0, 0, 0, 0, 0, 0]
+from .cacho import Cacho
 
-    def contar_pintas(self):
+class Contador_pintas:
+    def __init__(self, cacho: Cacho):
+        self.cacho = cacho
+
+    def contar_pintas(self, pinta: str, obligar: bool):
+        self.obligar = obligar
+        cont = 0
+        ases = 0
         for dado in self.cacho.dados:
             if dado.pinta() == "As":
-                self.contador[0] += 1
-            elif dado.pinta() == "Tonto":
-                self.contador[1] += 1
-            elif dado.pinta() == "Tren":
-                self.contador[2] += 1
-            elif dado.pinta() == "Cuadra":
-                self.contador[3] += 1
-            elif dado.pinta() == "Quina":
-                self.contador[4] += 1
-            elif dado.pinta() == "Sexto":
-                self.contador[5] += 1
-
+                ases += 1
+            elif dado.pinta() == pinta:
+                cont += 1
+            
         if not self.obligar:
-            for contador in range(1, 6):
-                self.contador[contador] += self.contador[0]
-        return self.contador
+            cont += ases
+        return cont
--- testing/test_contador_pintas.py
+++ testing/test_contador_pintas.py
@@ -3,5 +3,5 @@ from juego.cacho import Cacho
 
 def test_contador_pintas():
     cacho = Cacho()
-    contador = Contador_pintas(False, cacho)
-    assert contador.contar_pintas() is not None
+    contador = Contador_pintas(cacho)
+    assert contador.contar_pintas("Tonto", False) is not None
