68707caca5497468b4bbe19cf95989d419087952
Nikolexion <nfalc2003@gmail.com>
2025-08-28 10:12:02 -0400

    Red: test para conseguir la pinta del dado - falla como esperado

--- testing/test_dado.py
+++ testing/test_dado.py
@@ -1,8 +1,14 @@
 from juego.dado import Dado
 
-def test_dado():
+def test_lanzar_dado():
     dado = Dado()
 
     for _ in range(100):
         resultado = dado.lanzar()
         assert 1 <= resultado <= 6
+
+def test_pinta_dado():
+    dado = Dado()
+    dado.valor = 3
+    assert dado.pinta() == "Tren"
+    
