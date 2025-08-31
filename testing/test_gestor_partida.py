ce6656d25487d67b74c20c0945a1745386c9fd8d
amonlop <montlopanto@gmail.com>
2025-08-31 18:57:13 -0400

    RED: test para asignar nombre a jugador - falla como esperado

---
+++ testing/test_gestor_partida.py
@@ -0,0 +1,8 @@
+import pytest
+
+# 1. Para iniciar el juego, todos los jugadores lanzan un dado. El mayor parte, y en caso de empate, se vuelven a tirar los dados
+# 2. El jugador que parte decide el flujo de juego (horario o antihorario)
+# 3. El jugador que pierde o gana un dado empieza en la siguiente ronda
+# 4.
+
+
--- testing/test_jugador.py
+++ testing/test_jugador.py
@@ -4,6 +4,9 @@ from src.juego.jugador import Jugador
 
 #Crear jugador con 5 dados
 def test_inicializar_jugador():
-    jugador = Jugador()
+    jugador = Jugador("geoffrey")
     assert isinstance(jugador.cacho, Cacho)
     assert len(jugador.cacho.dados) == 5
+    assert jugador.nombre == "geoffrey"
+
+
