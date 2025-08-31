174d3d3ee3185763422291572b77668485585372
Rodrigo Bascuñán <rbascunan2023@udec.cl>
2025-08-30 20:04:43 -0400

    RED: test para decidir quien pierde/gana dado

--- testing/test_arbitro_ronda.py
+++ testing/test_arbitro_ronda.py
@@ -50,3 +50,21 @@ def test_resolver_calzar_bueno(mocker):
 
     resultado = arbitro.resolver_calzar(apuesta, cacho, jugador_calza=jugador2)
     assert resultado.acierta is True
+    assert resultado.pierden_dado == [jugador1]
+
+def test_calzar_incorrecto_pierde_quien_calza(mocker):
+    mock_generador = mocker.Mock()
+    mock_generador.generar.side_effect = [3, 2 , 1, 2, 2]
+    mocker.patch("src.juego.dado.GeneradorAleatorio", return_value=mock_generador)
+
+    jugador_apuesta = mocker.Mock()
+    jugador_calza = mocker.Mock()
+
+    arbitro = ArbitroRonda(0, [jugador_apuesta, jugador_calza])
+    apuesta = Apuesta(cantidad=3, pinta=3, jugador_que_aposto=jugador_apuesta)
+    cacho = Cacho()
+
+    resultado = arbitro.resolver_calzar(apuesta, cacho, jugador_calza=jugador_calza)
+
+    assert resultado.acierta is False
+    assert resultado.pierden_dado == [jugador_calza]
