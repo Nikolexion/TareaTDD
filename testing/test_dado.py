aa1eb48dcc5faa06b3fcb3b04345dceff900452d
amonlop <montlopanto@gmail.com>
2025-09-02 20:24:35 -0400

    REFACTOR: mejora en tests de dado

--- testing/test_dado.py
+++ testing/test_dado.py
@@ -1,4 +1,5 @@
 from src.juego.dado import Dado
+from src.servicios.generador_aleatorio import GeneradorAleatorio
 
 def test_lanzar_dado():
     dado = Dado()
@@ -12,3 +13,23 @@ def test_pinta_dado():
     dado.valor = 3
     assert dado.pinta() == "Tren"
     
+def test_dado_con_funcion_generadora():
+    def generador_mock():
+        return 4
+
+    dado = Dado(generador=generador_mock)
+    valor = dado.lanzar()
+
+    assert valor == 4
+    assert dado.pinta() == "Cuadra"
+
+
+def test_dado_con_generador_aleatorio_directo():
+    # Generador que siempre devuelve 6 (Sexto)
+    generador_fijo = GeneradorAleatorio(lambda: 6)
+
+    dado = Dado(generador=generador_fijo)
+    valor = dado.lanzar()
+
+    assert valor == 6
+    assert dado.pinta() == "Sexto"
